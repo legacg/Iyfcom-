2e18fdcbfd9c882ff9214b2c7d86b852294da1c9
legacg <Sirdiqsadickii@gmail.com>
2025-10-29 08:51:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+window.addEventListener('scroll', () => {
+  document.querySelectorAll('section').forEach(section => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 100) {
+      section.style.opacity = 1;
+      section.style.transform = 'translateY(0)';
+    }
+  });
+});
